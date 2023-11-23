13b4bc88ceed2025ce5004cc8d6756a4b48d64ba
Nguyễn Duy Nguyễn <20110530@student.hcmute.edu.vn>
2023-11-24 00:57:44 +0700

    update

--- server/Router/UploadRouter.js
+++ server/Router/UploadRouter.js
@@ -26,23 +26,4 @@ router.post('/uploads', uploadFile.array('images', 20), async (req, res) => {
     return res.status(500).json({ success: false, error: 'Lỗi trong quá trình tải lên.' });
   }
 });
-
-router.post('/uploadUser/:userID', uploadFile.single('image'), async (req, res) => {
-    try {
-      const { userID } = req.params;
-      if (!req.file) {
-        return res.status(400).json({ success: false, error: 'Không có ảnh được tải lên.' });
-      }
-      const user = await User.findById(userID);
-      if (!user) {
-        return res.status(404).json({ success: false, error: 'Người dùng không tồn tại.' });
-      }
-      user.avatar = req.file.path;
-      await user.save();
-      return res.status(200).json({ success: true, message: 'Tải lên ảnh thành công.', imageUrl: req.file.path });
-    } catch (error) {
-      console.error('Lỗi:', error);
-      return res.status(500).json({ success: false, error: 'Lỗi trong quá trình tải lên.' });
-    }
-  });
 module.exports = router;
